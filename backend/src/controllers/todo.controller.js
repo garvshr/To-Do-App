770d03e1d91ce1f7c7aa953630690b4de7285921
garvshr <garvsharma40@gmail.com>
2026-03-30 15:44:07 +0530

    [Garv] Add : Added update and delete controllers

--- backend/src/controllers/todo.controller.js
+++ backend/src/controllers/todo.controller.js
@@ -17,7 +17,29 @@ const getTodos = async (req, res) => {
     }
 };
 
+const updateTodos = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updateTodo = await Todo.findByIdAndUpdate(id, req.body, { new: true });
+        res.status(200).json({ message: "todo updated", updateTodo });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const deleteTodo = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedTodo = await Todo.findByIdAndDelete(id);
+        res.status(200).json({ message: "todo deleted", deletedTodo });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
 module.exports = {
     createTodo,
-    getTodos
+    getTodos, 
+    updateTodos,
+    deleteTodo
 };
--- backend/src/routes/todo.routes.js
+++ backend/src/routes/todo.routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
-const { getTodos, createTodo } = require('../controllers/todo.controller');
+const { getTodos, createTodo, updateTodos, deleteTodo} = require('../controllers/todo.controller');
+
 
 router.get('/', getTodos);
-router.post('/', createTodo);
+router.post('/create', createTodo);
+router.put('/update/:id', updateTodos);
+router.delete('/delete/:id', deleteTodo);
 
 module.exports = router;
